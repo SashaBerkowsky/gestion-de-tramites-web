46844dadc2e79dbca8f243cec8988d1dcc1db179
Priscila Bey <pri.bey@gmail.com>
2021-09-25 18:45:40 -0300

    clean table component from unused code

--- src/components/PendingTable.jsx
+++ src/components/PendingTable.jsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import PropTypes from "prop-types";
-// import { alpha } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -10,20 +9,13 @@ import TableHead from "@mui/material/TableHead";
 import TablePagination from "@mui/material/TablePagination";
 import TableRow from "@mui/material/TableRow";
 import TableSortLabel from "@mui/material/TableSortLabel";
-// import Toolbar from "@mui/material/Toolbar";
-// import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
-// import Checkbox from "@mui/material/Checkbox";
-// import IconButton from "@mui/material/IconButton";
-// import Tooltip from "@mui/material/Tooltip";
-// import FormControlLabel from "@mui/material/FormControlLabel";
-// import Switch from "@mui/material/Switch";
-// import DeleteIcon from "@mui/icons-material/Delete";
-// import FilterListIcon from "@mui/icons-material/FilterList";
 import { visuallyHidden } from "@mui/utils";
 import { Button } from "@mui/material";
 import moment from "moment";
 import "moment/locale/es";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function createData(code, type, userName, dni, createdAt) {
   return {
@@ -193,14 +185,7 @@ const headCells = [
 ];
 
 function EnhancedTableHead(props) {
-  const {
-    // onSelectAllClick,
-    order,
-    orderBy,
-    // numSelected,
-    // rowCount,
-    onRequestSort,
-  } = props;
+  const { order, orderBy, onRequestSort } = props;
   const createSortHandler = (property) => (event) => {
     onRequestSort(event, property);
   };
@@ -208,17 +193,6 @@ function EnhancedTableHead(props) {
   return (
     <TableHead>
       <TableRow>
-        {/* <TableCell padding="checkbox">
-          <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{
-              "aria-label": "select all desserts",
-            }}
-          />
-        </TableCell> */}
         {headCells.map((headCell) => (
           <TableCell
             key={headCell.id}
@@ -240,28 +214,8 @@ function EnhancedTableHead(props) {
             </TableSortLabel>
           </TableCell>
         ))}
-        <TableCell padding="checkbox">
-          {/* <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{
-              "aria-label": "select all desserts",
-            }}
-          /> */}
-        </TableCell>
-        <TableCell padding="checkbox">
-          {/* <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{
-              "aria-label": "select all desserts",
-            }}
-          /> */}
-        </TableCell>
+        <TableCell padding="checkbox"></TableCell>
+        <TableCell padding="checkbox"></TableCell>
       </TableRow>
     </TableHead>
   );
@@ -276,106 +230,18 @@ EnhancedTableHead.propTypes = {
   rowCount: PropTypes.number.isRequired,
 };
 
-// const EnhancedTableToolbar = (props) => {
-//   const { numSelected } = props;
-
-//   return (
-//     <Toolbar
-//       sx={{
-//         pl: { sm: 2 },
-//         pr: { xs: 1, sm: 1 },
-//         ...(numSelected > 0 && {
-//           bgcolor: (theme) =>
-//             alpha(
-//               theme.palette.primary.main,
-//               theme.palette.action.activatedOpacity
-//             ),
-//         }),
-//       }}
-//     >
-//       {numSelected > 0 ? (
-//         <Typography
-//           sx={{ flex: "1 1 100%" }}
-//           color="inherit"
-//           variant="subtitle1"
-//           component="div"
-//         >
-//           {numSelected} selected
-//         </Typography>
-//       ) : (
-//         <Typography
-//           sx={{ flex: "1 1 100%" }}
-//           variant="h6"
-//           id="tableTitle"
-//           component="div"
-//         >
-//           Nutrition
-//         </Typography>
-//       )}
-
-//       {numSelected > 0 ? (
-//         <Tooltip title="Delete">
-//           <IconButton>
-//             <DeleteIcon />
-//           </IconButton>
-//         </Tooltip>
-//       ) : (
-//         <Tooltip title="Filter list">
-//           <IconButton>
-//             <FilterListIcon />
-//           </IconButton>
-//         </Tooltip>
-//       )}
-//     </Toolbar>
-//   );
-// };
-
-// EnhancedTableToolbar.propTypes = {
-//   numSelected: PropTypes.number.isRequired,
-// };
-
 export default function PendingTable() {
   const [order, setOrder] = React.useState("asc");
   const [orderBy, setOrderBy] = React.useState("calories");
-  const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
-  //   const [dense, setDense] = React.useState(false);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  let history = useHistory();
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === "asc";
     setOrder(isAsc ? "desc" : "asc");
     setOrderBy(property);
   };
-
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      const newSelecteds = rows.map((n) => n.name);
-      setSelected(newSelecteds);
-      return;
-    }
-    setSelected([]);
-  };
-
-  //   const handleClick = (event, name) => {
-  //     const selectedIndex = selected.indexOf(name);
-  //     let newSelected = [];
-
-  //     if (selectedIndex === -1) {
-  //       newSelected = newSelected.concat(selected, name);
-  //     } else if (selectedIndex === 0) {
-  //       newSelected = newSelected.concat(selected.slice(1));
-  //     } else if (selectedIndex === selected.length - 1) {
-  //       newSelected = newSelected.concat(selected.slice(0, -1));
-  //     } else if (selectedIndex > 0) {
-  //       newSelected = newSelected.concat(
-  //         selected.slice(0, selectedIndex),
-  //         selected.slice(selectedIndex + 1)
-  //       );
-  //     }
-
-  //     setSelected(newSelected);
-  //   };
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -386,11 +252,10 @@ export default function PendingTable() {
     setPage(0);
   };
 
-  //   const handleChangeDense = (event) => {
-  //     setDense(event.target.checked);
-  //   };
-
-  //   const isSelected = (name) => selected.indexOf(name) !== -1;
+  const handleEvaluar = (code) => {
+    // TODO asignar el caso al usuario
+    history.push(`/in-progress/${code}`);
+  };
 
   // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
@@ -399,7 +264,6 @@ export default function PendingTable() {
   return (
     <Box sx={{ width: "100%" }}>
       <Paper sx={{ width: "100%", mb: 2 }}>
-        {/* <EnhancedTableToolbar numSelected={selected.length} /> */}
         <TableContainer>
           <Table
             sx={{ minWidth: 750 }}
@@ -407,10 +271,8 @@ export default function PendingTable() {
             size={"small"}
           >
             <EnhancedTableHead
-              numSelected={selected.length}
               order={order}
               orderBy={orderBy}
-              onSelectAllClick={handleSelectAllClick}
               onRequestSort={handleRequestSort}
               rowCount={rows.length}
             />
@@ -420,37 +282,9 @@ export default function PendingTable() {
               {stableSort(rows, getComparator(order, orderBy))
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row, index) => {
-                  //   const isItemSelected = isSelected(row.name);
-                  //   const labelId = `enhanced-table-checkbox-${index}`;
-
                   return (
-                    <TableRow
-                      //   hover
-                      //   onClick={(event) => handleClick(event, row.name)}
-                      //   role="checkbox"
-                      //   aria-checked={isItemSelected}
-                      tabIndex={-1}
-                      key={row.name}
-                      //   selected={isItemSelected}
-                    >
-                      {/* <TableCell padding="checkbox">
-                        <Checkbox
-                          color="primary"
-                          checked={isItemSelected}
-                          inputProps={{
-                            "aria-labelledby": labelId,
-                          }}
-                        />
-                      </TableCell> */}
-                      <TableCell
-                        // component="th"
-                        // id={labelId}
-                        // scope="row"
-                        // padding="none"
-                        align="left"
-                      >
-                        {row.code}
-                      </TableCell>
+                    <TableRow tabIndex={-1} key={row.name}>
+                      <TableCell align="left">{row.code}</TableCell>
                       <TableCell align="left">{row.type}</TableCell>
                       <TableCell align="left">{row.userName}</TableCell>
                       <TableCell align="left">{row.dni}</TableCell>
@@ -461,8 +295,10 @@ export default function PendingTable() {
                         <Button
                           color="secondary"
                           size="small"
-                          onClick={() => console.log("ver detalle")}
+                          //   onClick={() => console.log("ver detalle")}
                           sx={{ width: "max-content" }}
+                          component={Link}
+                          to={`/detail/${row.code}`}
                         >
                           Ver Detalle
                         </Button>
@@ -472,7 +308,7 @@ export default function PendingTable() {
                           color="secondary"
                           size="small"
                           variant="contained"
-                          onClick={() => console.log("evaluar")}
+                          onClick={() => handleEvaluar(row.code)}
                         >
                           Evaluar
                         </Button>
@@ -502,10 +338,6 @@ export default function PendingTable() {
           onRowsPerPageChange={handleChangeRowsPerPage}
         />
       </Paper>
-      {/* <FormControlLabel
-        control={<Switch checked={dense} onChange={handleChangeDense} />}
-        label="Dense padding"
-      /> */}
     </Box>
   );
 }
