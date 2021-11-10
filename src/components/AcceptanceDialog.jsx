f98e23908055b44ce6d5319f68a24843d1a2ac86
Sasha Berkowsky <s.nberkowsky@gmail.com>
2021-11-10 20:45:36 -0300

    quick fixes

--- src/components/AcceptanceDialog.jsx
+++ src/components/AcceptanceDialog.jsx
@@ -16,90 +16,90 @@ import { putUserResponsible } from "../api/procedures";
 import { useQuery, useMutation } from "react-query";
 
 function AcceptanceDialog({ onClose, open, idProcedure }) {
-  const [selectedPerson, setSelectedPerson] = useState("");
+	const [selectedPerson, setSelectedPerson] = useState("");
 
-  const handleChange = (event) => {
-    setSelectedPerson(event.target.value);
-  };
+	const handleChange = (event) => {
+		setSelectedPerson(event.target.value);
+	};
 
-  const handleClose = (isSuccess) => {
-    onClose(isSuccess);
-  };
+	const handleClose = (isSuccess) => {
+		onClose(isSuccess, "Usuario responsable asignado con exito!");
+	};
 
-  const {
-    isLoading,
-    data: usersResponsible,
-    isError,
-    error,
-  } = useQuery(["getUsersResponsible"], () => getUsersResponsible());
+	const {
+		isLoading,
+		data: usersResponsible,
+		isError,
+		error,
+	} = useQuery(["getUsersResponsible"], () => getUsersResponsible());
 
-  const {
-    mutateAsync: putUserMutation,
-    isLoading: isMutationLoading,
-    isError: isErrorInMutation,
-  } = useMutation(
-    (mutationData) => {
-      putUserResponsible(mutationData.idProcedure, mutationData.selectedPerson);
-    },
-    {
-      onError: () => {
-        alert("error");
-      },
-    }
-  );
+	const {
+		mutateAsync: putUserMutation,
+		isLoading: isMutationLoading,
+		isError: isErrorInMutation,
+	} = useMutation(
+		(mutationData) => {
+			putUserResponsible(mutationData.idProcedure, mutationData.selectedPerson);
+		},
+		{
+			onError: () => {
+				alert("error");
+			},
+		}
+	);
 
-  if (isLoading || isMutationLoading) return <Loader />;
+	if (isLoading || isMutationLoading) return <Loader />;
 
-  if (isError) return <ErrorAlert message={error.message} />;
+	if (isError) return <ErrorAlert message={error.message} />;
 
-  if (isErrorInMutation)
-    return <ErrorAlert message="Error al asignar usuario responsable" />;
+	if (isErrorInMutation)
+		return <ErrorAlert message='Error al asignar usuario responsable' />;
 
-  return (
-    <Dialog onClose={handleClose} open={open}>
-      <DialogTitle>Seleccione al responsable de aprobación</DialogTitle>
-      <DialogContent>
-        <DialogContentText id="alert-dialog-description">
-          <FormControl variant="standard" sx={{ m: 1, minWidth: 220 }}>
-            <InputLabel id="demo-simple-select-standard-label">
-              Responsable
-            </InputLabel>
-            <Select
-              labelId="demo-simple-select-standard-label"
-              id="demo-simple-select-standard"
-              value={selectedPerson}
-              onChange={handleChange}
-              label="Evaluador"
-            >
-              {usersResponsible.map((userRes, idx) => (
-                <MenuItem
-                  key={idx}
-                  value={userRes.id}
-                >{`${userRes.name} ${userRes.surname}`}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions sx={{ padding: 2 }}>
-        <Button onClick={handleClose} color="info" variant="contained">
-          Cancelar
-        </Button>
-        <Button
-          onClick={async () => {
-            await putUserMutation({ idProcedure, selectedPerson });
-            handleClose(true, "El usuario responsable fue asignado con exito!");
-          }}
-          autoFocus
-          color="secondary"
-          variant="contained"
-          disabled={!selectedPerson || isMutationLoading}
-        >
-          Aprobar
-        </Button>
-      </DialogActions>
-    </Dialog>
-  );
+	return (
+		<Dialog onClose={handleClose} open={open}>
+			<DialogTitle>Seleccione al responsable de aprobación</DialogTitle>
+			<DialogContent>
+				<DialogContentText id='alert-dialog-description'>
+					<FormControl variant='standard' sx={{ m: 1, minWidth: 220 }}>
+						<InputLabel id='demo-simple-select-standard-label'>
+							Responsable
+						</InputLabel>
+						<Select
+							labelId='demo-simple-select-standard-label'
+							id='demo-simple-select-standard'
+							value={selectedPerson}
+							onChange={handleChange}
+							label='Evaluador'
+						>
+							{usersResponsible.map((userRes, idx) => (
+								<MenuItem
+									key={idx}
+									value={userRes.id}
+								>{`${userRes.name} ${userRes.surname}`}</MenuItem>
+							))}
+						</Select>
+					</FormControl>
+				</DialogContentText>
+			</DialogContent>
+			<DialogActions sx={{ padding: 2 }}>
+				<Button onClick={handleClose} color='info' variant='contained'>
+					Cancelar
+				</Button>
+				<Button
+					onClick={async () => {
+						await putUserMutation({ idProcedure, selectedPerson });
+						handleClose(true, "El usuario responsable fue asignado con exito!");
+					}}
+					autoFocus
+					color='secondary'
+					variant='contained'
+					disabled={!selectedPerson || isMutationLoading}
+				>
+					Aprobar
+				</Button>
+			</DialogActions>
+		</Dialog>
+	);
 }
 
 export default AcceptanceDialog;
